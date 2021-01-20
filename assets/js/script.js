1af76ceade51ec76ad46ef6306fdb34af7e36723
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-01-20 16:27:37 -0500

    pseudo-coding functionality

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//============ Time and Date API =====
+// apiKey = 
+// queryURL =
+
+//============ Giphy API =============
+// apiKey = 
+// queryURL =
+
+// =========== FUNCTIONS =============
+//------------ Calendar --------------
+// Months toggle previous/next
+// Days can be selected to display in planner
+
+//------------ Planner ---------------
+//Reminder, date, and time are entered and saved in local storage: saveReminder()
+//Reminder does not disappear when page is refreshed.
+//Time slots reflect the hours of the day in real time: loadRows()
