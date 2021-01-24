24f573eaeab8d54347e68ea4429aa7105437c5db
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-01-24 01:06:49 -0500

    Code cleanup and comments added

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,26 +1,32 @@
-const api_key="u2tXL4wQEEJaa0E6OKHgypM1jhEthjjs"
-const searchEndpoint = "api.giphy.com/v1/gifs/search"
+const api_key="u2tXL4wQEEJaa0E6OKHgypM1jhEthjjs";
+const searchEndpoint = "api.giphy.com/v1/gifs/search";
 
-let searchQuery = "knowledge"
+// Sets search query to be "knowledge"
+let searchQuery = "knowledge";
 
+// Does GET XHR request for giphy API for searchQuery
 fetch(`http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchQuery}`)
     .then((response) => {
         if (response.ok) {
             console.log(response);
-            return response.text()
+            // Pass response as string if successful
+            return response.text();
         } else {
             console.log("This Failed?!");
         }
 })
 .then((responseData) => {
     const parsedData = JSON.parse(responseData);
-    const currentPageData = parsedData.data
-    console.log(currentPageData)
+    const currentPageData = parsedData.data;
+    console.log(currentPageData);
     
-
+    // Pick a random number between 0 and 50
     const randomNumber = Math.floor(Math.random() * Math.floor(50));
+
+    // Sets the image to be a random image from the giphy api
     const image = currentPageData[randomNumber].images.original.url;
 
+    // Attaches image to style attribute of gif container
     document.getElementById('gif').style.backgroundImage=`url(${image})`;
 })
 
